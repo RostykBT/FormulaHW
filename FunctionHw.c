f6483240095f12a2592e1b1eb64c7e6460690ac2
Rostyk Boychuk <rostykboychuk@gmail.com>
2022-09-20 15:43:09 +0300

    devidec by functions

--- FunctionHw.c
+++ FunctionHw.c
@@ -2,47 +2,94 @@
 #include <math.h>
 
 float getY(float);
+float getX(float, float, float);
+int getPointAvailability(float);
+float inputNumber(char[]);
+float getStepSize(float, float, float);
+void printRow(float, float, int);
 
 int main(void)
 {
     // declare all the required variables
-    float A, B, N;
+    
     float x;
-    float H;
     float y;
 
-    // input values
-    printf("Enter A: ");
-    scanf("%f", &A);
+    float A = inputNumber("insert A: ");
+    float B = inputNumber("insert B: ");
+    float N = inputNumber("insert N: ");
 
-    printf("Enter B: ");
-    scanf("%f", &B);
+    float H = getStepSize(A, B, N);
 
-    printf("Enter N: ");
-    scanf("%f", &N);
+    int points = N;
 
-    H = (B - A) / (N - 1); // calculate the step size
+    if(points > 15){
+        points = 15;
+    }
 
-    for (int i = 0; i < N && i < 15; i++)
+    float xs[points];
+    float ys[points];
+    int availabilities[points];
+
+    for (int i = 0; i < points; i++)
     {
-        x = A + i * H; // calculate the x value for ecah step
-
-        if (x == 0) // exclde the case of x = 0, as the value of y is undefined
-        {
-            printf("%f\t", x);
-            printf("not avilable\n");
-        }
-        else
-        {   
-            printf("%f\t", x);
-            printf("%f\n", getY(x));
+        x = getX(A, H, i); // calculate the x value for ecah step
+        xs[i] = x;
+        availabilities[i] = getPointAvailability(x);
+        if(availabilities[i] == 1){
+            y = getY(x); // calculate the y value for each step
+            ys[i] = y;
+        } else {
+            ys[i] = y;
         }
     }
+
+    for (int i = 0; i < points; i++)
+    {
+        printRow(xs[i], ys[i], availabilities[i]);
+    }
     return 0;
+
 }
+
+float inputNumber(char message[])
+{
+    float number;
+    printf("%s", message);
+    scanf("%f", &number);
+    return number;
+}
+
+float getStepSize(float A, float B, float N)
+{
+    float H = (B - A) / (N - 1);
+    return H;
+}
+
+float getX(float A, float H, float i)
+{
+    float x = A + H * i;
+    return x;
+}
+
+int getPointAvailability(float x){
+    if(x == 0){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 float getY(float x){
     float y;
     y = x + (x / (x + (pow(x, 2) / (x + ((pow(x, 3)) / 5)))));
     return y;
 }
 
+void printRow(float x, float y, int availability){
+    if(availability == 1){
+        printf("%f\t%f\n", x, y);
+    } else {
+        printf("%f\t%s", x, "N/A");
+    }
+}
